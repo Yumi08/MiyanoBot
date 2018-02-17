ce987d4a8cfa2b56f887fdf235cc2856b63f1e09
Yumi Ta <34748443+Yumi08@users.noreply.github.com>
2018-02-17 01:01:59 -0500

    End of Episode 4

--- MiyanoBot/Modules/Misc.cs
+++ MiyanoBot/Modules/Misc.cs
@@ -16,14 +16,32 @@ namespace MiyanoBot.Modules
 			var embed = new EmbedBuilder();
 			embed.WithTitle("Echoed message");
 			embed.WithDescription(message);
-			embed.WithColor(new Color(220, 20, 60));
+			embed.WithColor(new Color(223, 193, 159));
 
 			await Context.Channel.SendMessageAsync("", false, embed);
 		}
+
 		[Command("hello")]
 		public async Task Hello()
 		{
 			await Context.Channel.SendMessageAsync($"Hello {Context.User.Username}!");
+		}
+
+		[Command("pick")]
+		public async Task PickOne([Remainder]string message)
+		{
+			string[] options = message.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+
+			Random r = new Random();
+			string selection = options[r.Next(0, options.Length)];
+			
+			var embed = new EmbedBuilder();
+			embed.WithTitle("Choice for " + Context.User.Username);
+			embed.WithDescription(selection);
+			embed.WithColor(new Color(223, 193, 159));
+			embed.WithThumbnailUrl(Context.User.GetAvatarUrl());
+
+			await Context.Channel.SendMessageAsync("", false, embed);
 		}
 	}
 }
